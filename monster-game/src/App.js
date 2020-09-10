667761e11e5a1b737ee5d36f63ceadf11940427e
Neyko641 <shendamere@gmail.com>
2020-09-10 22:01:39 +0300

    Added images, and e-mails and made the component flexable

--- monster-game/src/App.js
+++ monster-game/src/App.js
@@ -17,12 +17,7 @@ componentDidMount() {
  render() {
   return (
   <div className="App"> 
-    <CardList name='List'>
-    {this.state.monsters.map(monster => <h1 key={monster.id}>
-                                               {monster.name}
-                                               </h1> 
-                                               )};
-    </CardList>
+    <CardList monsters = {this.state.monsters}/>
   </div>
   );
  }
--- monster-game/src/components/card-list/card-list-component.jsx
+++ monster-game/src/components/card-list/card-list-component.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
 import './card-list-component.css';
-export const CardList  = (props) => { 
- return <div className='Card-List'>{props.children}</div>;
-};
+
+import { Card } from './Card/card-component';
+
+export const CardList  = (props) => ( 
+<div className='Card-List'> 
+ {props.monsters.map(monster => ( 
+ <Card key = {monster.id} monster={monster} />
+ ))};
+ </div>
+);
