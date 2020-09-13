58a73a7c1e1ef69bd00de9511f8c73b0d6ce57b2
Neyko641 <shendamere@gmail.com>
2020-09-13 21:47:10 +0300

    Part 1 of the React course app done

--- monster-game/src/App.js
+++ monster-game/src/App.js
@@ -16,16 +16,20 @@ componentDidMount() {
   .then(response => response.json())
   .then(users => this.setState({monsters: users}));
 }
+handleChange = e => {
+  this.setState({ searchField: e.target.value })
+}
  render() {
    const { monsters, searchField } = this.state;
    const filteredMonsters = monsters.filter(monster => 
     monster.name.toLowerCase().includes(searchField.toLowerCase()) 
     );
   return (
-  <div className="App"> 
+  <div className="App">
+    <h1>Monsters Rolodex Cards</h1> 
     <SearchBox
       placeholder='Search Monsters'
-      handleChange = {e => this.setState({ searchField: e.target.value })}
+      handleChange = {this.handleChange}
     />
     <CardList monsters = {filteredMonsters}/>
   </div>
